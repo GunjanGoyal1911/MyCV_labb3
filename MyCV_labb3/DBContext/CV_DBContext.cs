71610ffab87427f9480235570d763fd710a57abf
Gunjan Goyal <gunjangoyal1911@gmail.com>
2024-02-25 17:18:56 +0100

    Correction

--- MyCV_labb3/DBContext/CV_DBContext.cs
+++ MyCV_labb3/DBContext/CV_DBContext.cs
@@ -11,7 +11,6 @@ namespace MyCV_labb3.DBContext
 		protected override void OnConfiguring(DbContextOptionsBuilder options)
 		{
             options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CVDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
-            //options.UseSqlServer("Server = tcp:gunjancv.database.windows.net, 1433; Initial Catalog = CVDatabase; Persist Security Info = False; User ID = gunjanadmin; Password =Gunjan@1911; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
         }
     }
 }
